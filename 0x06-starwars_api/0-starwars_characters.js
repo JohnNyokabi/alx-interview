6849820949b70707a4798b459e0aebb6df85b960
John Nyokabi <jwnyokabi@gmail.com>
2023-08-10 05:35:52 +0300

    Star Wars mos movie characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* Script that prints all characters of a Star Wars movie */
+
+const request = require('request');
+const urlApi = 'https://swapi-api.hbtn.io/api/films/';
+const movieId = process.argv[2];
+
+request(urlApi + movieId, (error, response, body) => {
+  if (error) throw error;
+  const characters = JSON.parse(body).characters;
+  showNames(characters);
+});
+
+const showNames = (names, i = 0) => {
+  if (i === names.length) return;
+  request(names[i], (error, response, body) => {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    showNames(names, i + 1);
+  });
+};
