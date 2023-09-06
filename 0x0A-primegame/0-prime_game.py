4317e7399b65d823fa2c690ecea0fb940ac251b3
John Nyokabi <jwnyokabi@gmail.com>
2023-09-06 09:34:48 +0300

    python algorith using prime number game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,30 +3,46 @@
 """
 
 
-def isWinner(x, nums):
-    """ returns the winner between Ben and Maria"""
-    def is_prime(num):
-        """determines if a number is prime"""
-        if num < 2:
+def is_prime(n):
+    """determines if n is a prime number
+    """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
             return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+    return True
 
-    maria_wins = 0
-    ben_wins = 0
 
-    for n in nums:
-        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-        if prime_count % 2 == 0:
-            ben_wins += 1
+def add_prime(n, primes):
+    """ Add prime to list """
+    prime_count = primes[-1]
+    if n > last_prime:
+        for i in range(last_prime + 1, n + 1):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
+
+
+def isWinner(x, nums):
+    """returns the winner between Maria and Ben
+    """
+    score = {"Maria": 0, "Ben": 0}
+    primes = [0, 0, 2]
+    add_prime(max(nums), primes)
+
+    for round in range(x):
+        _sum = sum((i != 0 and i <= nums[round])
+                   for i in primes[:nums[round] + 1])
+        if (_sum % 2):
+            winner = "Maria"
         else:
-            maria_wins += 1
+            winner = "Ben"
+        if winner:
+            score[winner] += 1
 
-    if maria_wins > ben_wins:
+    if score["Maria"] > score["Ben"]:
         return "Maria"
-    elif maria_wins < ben_wins:
+    elif score["Ben"] > score["Maria"]:
         return "Ben"
-    else:
-        return None
+
+    return None
